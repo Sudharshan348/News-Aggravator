6fb7eb62d42ea71ebdf790ba57c09cb476d37d6d
Sudharshan348 <bharathisudharshan348@gmail.com>
2025-07-26 15:16:58 +0530

    Bringing the Saved news from MonogDB

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config(); 
-
 import express from 'express';
 import newsRouter from './routes/news.js';
 import connectDB from './db/index.js'
+import savedNewsRoutes from './routes/SavedNews.js'
 
 connectDB()
 const app = express();
 app.use('/api', newsRouter);
+app.use('/api',savedNewsRoutes)
 app.get('/', (req,res) => {
     res.send('API is executing and backend is working')
 })
---
+++ backend/routes/SavedNews.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { Article } from '../models/article.js';
+
+const router = express.Router();
+
+router.get('/saved', async (req, res) => {
+  try {
+    const articles = await Article.find()
+      .sort({ publishedAt: -1 })
+      .limit(50);
+    res.json(articles);
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching saved articles" });
+  }
+});
+
+export default router;
