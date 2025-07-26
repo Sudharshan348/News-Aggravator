c5d6723d6e591054a2103320762756ea0e494b68
Sudharshan348 <bharathisudharshan348@gmail.com>
2025-07-26 15:08:10 +0530

    Solved the issue of articled being stored in MongoDB

--- backend/routes/news.js
+++ backend/routes/news.js
@@ -1,5 +1,6 @@
 import express from "express";
 import axios from "axios";
+import { Article } from '../models/article.js'
 const router = express.Router();
 
 router.get('/headlines', async (req, res) => {
@@ -11,7 +12,23 @@ router.get('/headlines', async (req, res) => {
       },
     }
     );
-    res.json(response.data);
+    const articles = response.data.articles;
+
+    for (let item of articles) {
+      const exists = await Article.findOne({ title: item.title });
+      if (!exists) {
+        const article = new Article({
+          title: item.title,
+          url: item.url,
+          source: item.source.name,
+          publishedAt: new Date(item.publishedAt),
+          content: item.content || "",
+        });
+        await article.save();
+      }
+    }
+
+    res.json({ message: "Articles fetched and stored", count: articles.length });
   } catch (error) {
     console.error('News API error:', error.message);
     res.status(500).json({
