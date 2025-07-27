4524bf69c3fe529ff48ed5ac68a84b54538cdeed
Sudharshan348 <bharathisudharshan348@gmail.com>
2025-07-27 21:36:52 +0530

    cors updated

--- news-backend/index.js
+++ news-backend/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors({
   origin: [
     'https://news-aggravator.vercel.app',
+    'https://news-aggravator-b-sudharshans-projects.vercel.app/',
     'http://localhost:5173', 
     'http://localhost:3000',
     'http://localhost:4173', 
